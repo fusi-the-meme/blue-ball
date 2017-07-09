1ef54852e0ea049b538b8b2f9bd09b1ad4b7ee15
fusi-the-meme <fusimeme@gmail.com>
2017-07-09 14:53:02 +0930

    Initial Import
    Initial Import of most files

---
+++ Assets/Scriptz/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Kai G was here
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// working out current obj
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// updating camera
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
